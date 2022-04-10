43da94ab6af08e30e128437a19b72598175b0496
CedricD62 <finalfantasy001@hotmail.com>
2022-04-10 15:31:11 +0200

    add mobie componant and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomepageViewModule } from './homepage-view/homepage-view.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { HomepageViewModule } from './homepage-view/homepage-view.module';
   ],
   imports: [
     BrowserModule,
-    HomepageViewModule
+    HomepageViewModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage-view/homepage-view.module.ts
+++ src/app/homepage-view/homepage-view.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageViewComponent } from './homepage-view.component';
 import { ListComponent } from './list/list.component';
-import { HeaderComponent } from '../shared/header/header.component';
 import { MovieHighlightDirective } from '../shared/movie-highlight.directive';
 import { FavoriteDirectorPipe } from '../shared/favorite-director.pipe';
 import { MovieComponent } from './list/movie/movie.component';
+import { HeaderModule } from '../shared/header/header.module';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -13,13 +16,16 @@ import { MovieComponent } from './list/movie/movie.component';
   declarations: [
     HomepageViewComponent,
     ListComponent,
-    HeaderComponent,
     MovieHighlightDirective,
     FavoriteDirectorPipe,
     MovieComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports :[
     HomepageViewComponent
